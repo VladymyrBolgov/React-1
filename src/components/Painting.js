332a630024a6c292429adccb0ca4c8c1d4b1d2a8
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-05 21:53:08 +0300

    компоненты 1

--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,17 +1,16 @@
 import PropTypes from 'prop-types';
-import s from './Painting.module.css';
 import defaultImage from './default.jpg';
 
 const Painting = ({
   imageUrl = defaultImage,
   title,
   profileUrl,
-  author,
+  author = 'не известно',
   price,
   quantity,
 }) => {
   return (
-    <div className={s.container}>
+    <div>
       <img src={imageUrl} alt={title} width="480" />
       <h2>{title}</h2>
       <p>
